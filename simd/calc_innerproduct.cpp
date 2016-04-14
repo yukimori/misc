f6e6f190b9e75b101b9f3e3f72f483745b970cd0
yuhara <yuharacomplex@gmail.com>
2016-04-15 08:59:27 +0900

    stopwatch関数を導入(jubatus-benchを参考に) 既存の時間計測コードは100回試行して平均時間を計算する形に変更
    TODO:
    stopwatch関数でも複数回の測定を行い平均時間を算出する
    stopwatch関数での測定時間と既存計測コードでの測定時間に倍以上の差がある

--- simd/calc_innerproduct.cpp
+++ simd/calc_innerproduct.cpp
@@ -14,6 +14,8 @@ osakana/stopwatchは非公開ライブライリなので置き換える必要が
 
 20160414 動作環境がavx非対応なのでnormalとsseのみ
 sseの方が時間がかかる
+
+g++ -std=c++11 -Wall calc_innerproduct.cpp -O3 -msse4.2 -o calc_innerproduct
  */
 
 float dot_normal(const float *vec1, const float *vec2, unsigned n)
@@ -44,12 +46,24 @@ float dot_sse(const float *vec1, const float *vec2, unsigned n)
   return sum;
 }
 
-void print_result(int len, std::string type, int elapsed, float sum) {
+void print_result(int len, std::string type, double elapsed, float sum=0.0) {
   std::cout << std::setw(12) << std::right << len << ",";
   std::cout << std::setw(6) << std::right << type << ",";
   std::cout << std::setw(12) << std::right << elapsed << ",";
   std::cout << std::setw(10) << std::right << sum << std::endl;
 }
+
+template< typename F, class Period = std::ratio<1> >
+std::chrono::duration<double, Period> stopwatch(F&& func) {
+  auto start = std::chrono::high_resolution_clock::now();
+  func();
+  return std::chrono::duration_cast<std::chrono::duration<double, Period> >(std::chrono::high_resolution_clock::now() - start);
+}
+
+// template<class T>
+// double calc_for_a_moment(T t, unsined ms) {
+  
+// }
 
 int main()
 {
@@ -60,33 +74,45 @@ int main()
   std::mt19937 rng;
   std::uniform_real_distribution<> dst(-1, 1);
 
-  std::cout << "size,{sse,normal},elapsedtime[micro seconds],sum" << std::endl;
+  typedef std::chrono::duration<double, std::ratio<1, 1000> > Milli ;
+
+  std::cout << "size,{sse,normal},elapsedtime[ms],sum" << std::endl;
 
   for(unsigned len = len_begin; len <= len_end; len *= len_fact)
 {
   float* p1;
-  posix_memalign((void**)&p1, 16, (len+8)*sizeof(float));
+  posix_memalign((void**)&p1, 16, (len)*sizeof(float));
   float* p2;
-  posix_memalign((void**)&p2, 16, (len+8)*sizeof(float));
+  posix_memalign((void**)&p2, 16, (len)*sizeof(float));
   float *vec1 = p1;
   float *vec2 = p2;
 
   std::generate(vec1, vec1+len, [&rng, &dst](){return dst(rng);});
   std::generate(vec2, vec2+len, [&rng, &dst](){return dst(rng);});
   
+  Milli sse_time = stopwatch([vec1, vec2, len](){
+  	  return dot_sse(vec1, vec2, len);
+  	});
+  print_result(len, "sse", sse_time.count());
 
+  float sum = 0.0;
+  int N = 100;
   auto time_point = std::chrono::high_resolution_clock::now();
-  float sum = dot_sse(vec1, vec2, len);
+  for (int i=0; i<N; ++i) {
+	sum = dot_sse(vec1, vec2, len);
+  }
   auto duration = std::chrono::high_resolution_clock::now() - time_point;
   auto micros = std::chrono::duration_cast<std::chrono::microseconds>(duration);
-  print_result(len, "sse", micros.count(), sum);
+  print_result(len, "sse", (micros.count()/N), sum);
   
-  
+
   time_point = std::chrono::high_resolution_clock::now();
-  sum = dot_normal(vec1, vec2, len);
+  for (int i=0; i<N; ++i) {
+	sum = dot_normal(vec1, vec2, len);
+  }
   duration = std::chrono::high_resolution_clock::now() - time_point;
   micros = std::chrono::duration_cast<std::chrono::microseconds>(duration);
-  print_result(len, "normal", micros.count(), sum);
+  print_result(len, "normal", (micros.count()/N), sum);
 
 
   delete[] p1;
