868d4e8a86c7d110496cbdf97f98c015a2e17ab0
yuhara <yuharacomplex@gmail.com>
2016-05-11 06:41:00 +0900

    ディレクトリ再構成

---
+++ math/test_div.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int x=101;
+    int y=2;
+
+    std::cout << (x/y) << std::endl ;
+}
---
+++ optimization/test_optimization.c
@@ -0,0 +1,10 @@
+/*
+  ref:
+  http://long-tweet.tumblr.com/post/80136153973/%E6%9C%80%E8%BF%91%E3%83%84%E3%82%A4%E3%83%83%E3%82%BF%E3%83%BC%E3%81%A7-while1love-%E3%81%A8%E6%9B%B8%E3%81%8D%E8%BE%BC%E3%81%BE%E3%82%8C%E3%81%A6%E3%82%8B%E6%8C%87%E8%BC%AA%E3%81%8C%E3%81%82%E3%81%A3%E3%81%A6
+ */
+
+int main()
+{
+  int LOVE = 0;
+  while(1){LOVE++;}
+}
