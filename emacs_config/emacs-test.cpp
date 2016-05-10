3a76d8009872ea6894a57563e8497f648eb37fa0
yuhara <yuharacomplex@gmail.com>
2016-05-11 06:40:13 +0900

    emacs_configにemacsテスト関係のコードを一括配置

---
+++ emacs_config/emacs-test.cpp
@@ -0,0 +1,14 @@
+#include <float.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    int vx = 0;
+    while (vx != 10) {
+        // printf("This is good\n");
+        std::cout << "This is good" << "\n";
+        vx++;
+    }
+    return 0;
+}
+
+
