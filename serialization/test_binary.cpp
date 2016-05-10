7e046bfd2be109bc66af02a939420cdc81f886db
yuhara <yuharacomplex@gmail.com>
2016-05-11 06:42:54 +0900

    serialization関連のテストコードを追加

---
+++ serialization/test_binary.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+/**
+   http://gurigumi.s349.xrea.com/programming/binary.html
+ **/
+
+int test_binary() {
+    std::ofstream fout;
+    double a[100];
+    fout.open("file.txt", std::ios::out|std::ios:binary|std::ios::trunc);
+}
+
+int main() {
+
+}
