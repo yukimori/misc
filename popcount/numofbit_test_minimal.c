e08c1411a034bcc89ad37dd651249072a7f3b508
yuhara <yuharacomplex@gmail.com>
2016-07-20 19:48:44 +0900

    ベンチマーク対象の拡大

--- popcount/numofbit_test_minimal.c
+++ popcount/numofbit_test_minimal.c
@@ -15,6 +15,7 @@ enum {
 
 extern int dummy(uint32_t x) __attribute__((noinline));
 extern int numofbits1(uint32_t x) __attribute__((noinline));
+int numofbits5(uint32_t x);
 extern int popcount8if(unsigned int x) __attribute__((noinline));
 extern int popcount_16bit_table(unsigned int x) __attribute__((noinline));
 extern int _popcount_16bit_table_init();
@@ -31,6 +32,7 @@ int test(){
     printf("value check. loop:%d\n",REPEAT);
     printf("regard numofbits1 as correct\n");
     int ans_nob1 = 0;
+    int ans_nob5 = 0;
     int ans_8if = 0;
     int ans_16table = 0;
     int ans_popcntsse42 = 0;
@@ -40,6 +42,7 @@ int test(){
     int test_ok = 1;
     for(int i=0; i<REPEAT; i++) {
         ans_nob1 += numofbits1(data[i % DATA_SIZE]);
+        ans_nob5 += numofbits5(data[i % DATA_SIZE]);
         ans_8if += popcount8if(data[i % DATA_SIZE]);
         ans_16table += popcount_16bit_table(data[i % DATA_SIZE]);
         ans_popcntsse42 += popcount_32bitsse42(data[i % DATA_SIZE]);
@@ -47,6 +50,11 @@ int test(){
         if(ans_nob1 != ans_8if) {
             test_ok = 0;
             printf("[%d]popcount 8if expect:%d fact:%d value:%x\n",i,ans_nob1, ans_8if,data[i%DATA_SIZE]);
+            break;
+        }
+        if(ans_nob1 != ans_nob5) {
+            test_ok = 0;
+            printf("[%d]nomofbit5 expect:%d fact:%d value:%x\n",i,ans_nob1, ans_nob5,data[i%DATA_SIZE]);
             break;
         }
         if(ans_nob1 != ans_16table) {
@@ -72,6 +80,7 @@ int test(){
 
     printf("sum 1 bits\n");
     printf("numofbits1:%d\n", ans_nob1);
+    printf("numofbits5:%d\n", ans_nob5);
     printf("popcount 8if:%d\n", ans_8if);
     printf("popcount 16bit table:%d\n", ans_16table);
     printf("popcount 64bit sse4.2:%d\n", ans_popcnt64sse42);
@@ -139,6 +148,28 @@ int main(int argc, char** argv) {
     }
     gettimeofday(&after, NULL);
     print_time("popcount_32bitsse42", before, after);
+
+    int ans_popcnt64sse42 = 0;
+    gettimeofday(&before, NULL);
+    for (i=0; i<REPEAT; i+=2) {
+        ans_popcnt64sse42 = popcount_64bitsse42(data[i%DATA_SIZE],
+                                                 data[(i+1)%DATA_SIZE]);
+
+    }
+    gettimeofday(&after, NULL);
+    print_time("popcount_64bitsse42", before, after);
+
+    
+    // numofbits5
+    int ans_nob5 = 0;
+    gettimeofday(&before, NULL);
+    for (i=0 ; i<REPEAT ; i++) {
+        ans_nob5 = numofbits5(data[i % DATA_SIZE]);
+    }
+    gettimeofday(&after, NULL);
+    /* printf("numofbits1: %10.6f sec\n", getdiff(&before, &after) - base_time); */
+    printf("numofbits5: %10.6f sec\n", getdiff(&before, &after));
+
     
     // numofbits1
     int ans_nob1 = 0;
