7bd65da01cf57ddd41f873ca8fa0bf92181d5715
yuhara <yuharacomplex@gmail.com>
2016-07-23 07:55:43 +0900

    constの挙動を確かめるコード

---
+++ cpp/test_const.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int a = 0;
+
+    /* constポインタ */
+    const int* b = &a;
+    // int const* b = &a;  // 上の行と同義
+    // *b = 1;  //エラー bはconst intへのポインタ
+
+    // ポインタオブジェクトで保持するアドレスを不変にしたい場合
+    // bのアドレスが不変
+    // *bでアクセスするaの保持している値は書き換えることができる
+    int* const b2 = &a; 
+
+    /* const参照 */
+    int a3 = 10;
+    const int& b3 = a;
+    // b = 20;  // エラー:bはconst修飾されている
+
+    // int& const b4 = a; エラー
+}
